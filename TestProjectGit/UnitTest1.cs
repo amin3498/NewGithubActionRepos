07ae0d15d842eceff999a9fc0e1dbb984794f59a
moamin3498 <moamin3498@gmail.com>
2023-11-13 21:08:59 -0500

    Test

---
+++ TestProjectGit/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace TestProjectGit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TC001_TestMethod1()
+        {
+            Console.WriteLine("This is a test");
+        }
+        [TestMethod]
+        public void TC002_TestMethod1()
+        {
+            Console.WriteLine("This is a test");
+        }
+        [TestMethod]
+        public void TC003_TestMethod1()
+        {
+            Console.WriteLine("This is a test");
+        }
+    }
+}
